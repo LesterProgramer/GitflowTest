3364979bd59be688c76422bc79aa7ace2f79cdd4
Lester <programmer@lester.com.mx>
2025-08-27 16:04:17 -0600

    Prueba de integracion

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const readlineSync = require('readline-sync');
 
 const Saludo = () => {
-    console.log(`Esto es una prueba de saludo \n`);
-    const nombre = readlineSync.question('¿Cual es tu nombre?')
+    console.log(`Bienvenido Sistema de Desarrollo \n`);
+    const nombre = readlineSync.question('Cual es tu nombre? ')
 
-    console.log(`Hola ${nombre}`);
+    console.log(`Bienvenido ${nombre}`);
 } 
 
 Saludo();
